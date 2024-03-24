2d11b17b2d1747527f78c1b527f8fc49c25415f7
Laura <lauramb.morlans@gmail.com>
2024-03-24 12:10:32 +0100

    updated test

--- client/src/queries/getOffers.js
+++ client/src/queries/getOffers.js
@@ -3,15 +3,15 @@ import axios from "axios";
 export const getOffers = async () => {
   let baseURL = "";
 
-  console.log('process.env.NODE_ENV:', process.env.NODE_ENV)
+  console.log('process.env.REACT_APP_NODE_ENV:', process.env.REACT_APP_NODE_ENV)
 
   // Check if running in development mode
-  if (process.env.NODE_ENV === "development") {
-    console.log('hey')
-    baseURL = "http://localhost:8080";
+  if (process.env.REACT_APP_NODE_ENV === 'development') {
+    console.log('1')
+    baseURL = "http://localhost:8080"; // Your local Express server
   } else {
     // Set your production API URL here
-    console.log('yo')
+    console.log('2')
     baseURL = `${window.location.protocol}//${window.location.hostname}`;
   }
 
