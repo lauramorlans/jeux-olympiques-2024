c90374e8a6d0308bb3d94a91eee8d041162549ec
Laura <lauramb.morlans@gmail.com>
2024-03-24 11:42:59 +0100

    added server gitignore + updated get offers to have env

--- client/src/queries/getOffers.js
+++ client/src/queries/getOffers.js
@@ -1,7 +1,22 @@
 import axios from "axios";
 
 export const getOffers = async () => {
-  const response = await axios.create().get('http://localhost:8080/offers')
+  let baseURL = "";
+
+  // Check if running in development mode
+  if (process.env.NODE_ENV === "development") {
+    baseURL = "http://localhost:8080";
+  }
+  // else {
+  //   // Set your production API URL here
+  //   baseURL = "https://your-production-api-url.com";
+  // }
+
+  const instance = axios.create({
+    baseURL: baseURL
+  });
+
+  const response = await instance.get('/offers')
 
   return response.data;
 }
